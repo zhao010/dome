02055d3a11686ba71ac73ee264018f354b25b7a6
zhao010 <66990401+zhao010@users.noreply.github.com>
2020-12-08 16:14:23 +0800

    Update unit_test.py

--- python/oo/common_test/unit_test.py
+++ python/oo/common_test/unit_test.py
@@ -3,12 +3,6 @@
 #
 #
 
-
-import sys
-import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
 import unittest
 
 from common_test.selenium_driver import driver_Test, logg

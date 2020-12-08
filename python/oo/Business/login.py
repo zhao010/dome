41757a5cad96afda73787d1d91d8c60f6dc225d9
zhao010 <66990401+zhao010@users.noreply.github.com>
2020-12-08 16:11:32 +0800

    Update login.py

--- python/oo/Business/login.py
+++ python/oo/Business/login.py
@@ -1,11 +1,3 @@
-
-
-import sys
-import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
-
 from selenium.webdriver.common.by import By
 
 from pageobject_test.pageobject import PageObject_test

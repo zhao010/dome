5a6391fabe72cd4c54f57cffd8bec2745cfef6e2
zhao010 <66990401+zhao010@users.noreply.github.com>
2020-12-21 20:46:17 +0800

    Update run_testcase.py

--- python/oo/Run_case/run_testcase.py
+++ python/oo/Run_case/run_testcase.py
@@ -1,6 +1,6 @@
 
 import sys
-sys.path.append(r"/root/.jenkins/workspace/selenium_demo1/python")
+sys.path.append(r"/root/.jenkins/workspace/selenium_demo1/python/oo")
 
 import time
 import unittest
@@ -11,7 +11,7 @@ from Run_case import HTMLTestRunner
 if __name__ == '__main__':
     s = unittest.TestSuite()
     s.addTests(unittest.TestLoader().loadTestsFromTestCase(Login_xx))
-    path = '/oo/data'+str(time.strftime('%Y-%m-%d %H_%M_%S'))+'.html'
+    path = '/data'+str(time.strftime('%Y-%m-%d %H_%M_%S'))+'.html'
     file = open(path,'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=file,title='自动化测试报告',description='Ucloud登录')
     runner.run(s)

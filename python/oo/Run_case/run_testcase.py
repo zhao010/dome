b9c1de2f819aa3c5ec71e17600fab0e9ae6cafef
zhao010 <66990401+zhao010@users.noreply.github.com>
2020-12-21 20:30:51 +0800

    Update run_testcase.py

--- python/oo/Run_case/run_testcase.py
+++ python/oo/Run_case/run_testcase.py
@@ -11,7 +11,7 @@ from Run_case import HTMLTestRunner
 if __name__ == '__main__':
     s = unittest.TestSuite()
     s.addTests(unittest.TestLoader().loadTestsFromTestCase(Login_xx))
-    path = 'python/oo/data'+str(time.strftime('%Y-%m-%d %H_%M_%S'))+'.html'
+    path = '/oo/data'+str(time.strftime('%Y-%m-%d %H_%M_%S'))+'.html'
     file = open(path,'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=file,title='自动化测试报告',description='Ucloud登录')
     runner.run(s)
